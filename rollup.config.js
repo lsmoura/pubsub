bb8727036853f6819e9cca11b77c1abc9d048ed9
Sergio Moura <sergio@moura.ca>
2019-09-27 13:25:48 -0400

    packaging support

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+import babel from 'rollup-plugin-babel';
+
+module.exports = {
+  input: 'src/index.js',
+  output: {
+    dir: 'lib',
+    format: 'umd',
+    sourcemap: true,
+    indent: false,
+    name: 'pubsub',
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    resolve(),
+    commonjs(),
+    process.env.NODE_ENV === 'production' ? uglify() : null,
+  ],
+};
